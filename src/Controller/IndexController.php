8a653c5851ec887988a83e1b05e4025c597fce13
klim-sherstyankov <sherstianikov.klim@yandex.ru>
2022-01-16 18:17:08 +0300

    normal func

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -15,51 +15,26 @@ class IndexController extends AbstractController
      */
     public function index(Request $request)
     {
-        $array = [11, 2, 3, 4, 9, 1, 8, 66, 55, 22, 4];
+        $array  = [11, 2, 3, 4, 9, 1, 8, 66, 55, 22, 4];
         $bubble = $this->bubble($array);
-        $array2 = [11, 2, 3, 4, 9, 1, 8, 66, 55, 22, 4];
-        $stop = true;
-        while ($stop) {
-            $stop = false;
-            for ($i = 0; $i < count($array2) - 1; $i++) {
-                if ($array2[$i] > $array2[$i + 1]) {
-                    $temp = $array2[$i + 1];
-                    $array2[$i + 1] = $array2[$i];
-                    $array2[$i] = $temp;
-                    $stop = true;
-                }
-            }
-        }
 
-        $arraySheyk = [11, 2, 3, 4, 9, 1, 8, 66, 55, 22, 4];
-        $left = 0;
-        $right = count($arraySheyk) - 1;
-        while ($left <= $right) {
-            for ($i=$left; $i < $right ; ++$i) {
-                if($arraySheyk[$i + 1] < $arraySheyk[$i]) {
-                    $tmp = $arraySheyk[$i + 1];
-                    $arraySheyk[$i + 1] = $arraySheyk[$i];
-                    $arraySheyk[$i] = $tmp;
-                }
-            }
-            --$right;
-            for ($i=$right; $i > $left ; --$i) {
-                if($arraySheyk[$i - 1] > $arraySheyk[$i]) {
-                    $tmp = $arraySheyk[$i - 1];
-                    $arraySheyk[$i - 1] = $arraySheyk[$i];
-                    $arraySheyk[$i] = $tmp;
-                }
-            }
-            ++$left;
-        }
+        $array2       = [11, 2, 3, 4, 9, 1, 8, 66, 55, 22, 4];
+        $bubbleSecond = $this->bubbleSecond($array);
+
+        $arraySheyk   = [11, 2, 3, 4, 9, 1, 8, 66, 55, 22, 4];
+        $bubbleSecond = $this->sheyk($array);
 
         return $this->render('index/index.html.twig', [
-            'bubbleTime' => $bubble['time'],
-            'bubbleMemory' => $bubble['memory'],
-            'bubbleArray' => $bubble['array'],
-            'array2' => $array2,
-            'arraySheyk' => $arraySheyk,
-            'path'  => $request->getRequestUri(),
+            'bubbleTime'         => $bubble['time'],
+            'bubbleMemory'       => $bubble['memory'],
+            'bubbleArray'        => $bubble['array'],
+            'bubbleSecondTime'   => $bubbleSecond['time'],
+            'bubbleSecondMemory' => $bubbleSecond['memory'],
+            'bubbleSecondArray'  => $bubbleSecond['array'],
+            'sheykTime'          => $bubbleSecond['time'],
+            'sheykMemory'        => $bubbleSecond['memory'],
+            'sheykArray'         => $bubbleSecond['array'],
+            'path'               => $request->getRequestUri(),
         ]);
     }
 
@@ -76,7 +51,61 @@ class IndexController extends AbstractController
                 }
             }
         }
-        $time = microtime(true) - $startScript;
+        $time   = microtime(true) - $startScript;
+        $memory = memory_get_peak_usage();
+
+        return ['time' => $time, 'memory' => $memory, 'array' => $array];
+    }
+
+    public function bubbleSecond(array $array)
+    {
+        $startScript = microtime(true);
+
+        $stop = true;
+        while ($stop) {
+            $stop = false;
+            for ($i = 0; $i < count($array) - 1; $i++) {
+                if ($array[$i] > $array[$i + 1]) {
+                    $temp          = $array[$i + 1];
+                    $array[$i + 1] = $array[$i];
+                    $array[$i]     = $temp;
+                    $stop          = true;
+                }
+            }
+        }
+
+        $time   = microtime(true) - $startScript;
+        $memory = memory_get_peak_usage();
+
+        return ['time' => $time, 'memory' => $memory, 'array' => $array];
+    }
+
+    public function sheyk(array $array)
+    {
+        $startScript = microtime(true);
+
+        $left  = 0;
+        $right = count($array) - 1;
+        while ($left <= $right) {
+            for ($i = $left; $i < $right; ++$i) {
+                if ($array[$i + 1] < $array[$i]) {
+                    $tmp           = $array[$i + 1];
+                    $array[$i + 1] = $array[$i];
+                    $array[$i]     = $tmp;
+                }
+            }
+            --$right;
+            for ($i = $right; $i > $left; --$i) {
+                if ($array[$i - 1] > $array[$i]) {
+                    $tmp           = $array[$i - 1];
+                    $array[$i - 1] = $array[$i];
+                    $array[$i]     = $tmp;
+                }
+            }
+            ++$left;
+        }
+
+        $time   = microtime(true) - $startScript;
         $memory = memory_get_peak_usage();
 
         return ['time' => $time, 'memory' => $memory, 'array' => $array];
